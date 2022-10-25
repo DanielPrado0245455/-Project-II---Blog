9dbad44f691c0a6f871bc5c7b125da06692e9c32
DJ2513 <djontiveros2728@gmail.com>
2022-10-25 18:17:21 -0500

    fuciono
    Probando la applicacion

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+let ipath = path.join(__dirname,"public");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(ipath));
+app.use(fileUpload());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render(path.join(ipath, "/views/index"));    
+});
+
+app.get('/news', (req,res)=>{
+    res.render(path.join(ipath, '/views/news'))
+})
+
+app.get("/editor", (req, res) => {
+    res.render(path.join(ipath, "/views/editor"));   
+});
+
+app.get("/upload", (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    let imagename= date.getDate() + DataTransfer.getTime() + file.name;
+    let path= 'public/uploads' + imagename;
+
+    file.mv(path, (err, result) => {
+        if(err){
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
+        }
+    })
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
